fbb1644c7139c46f0a31a7337af4d9028cf29af3
Opilane TTHK <opilane@tthk.ee>
2020-09-24 12:41:29 +0300

    commit

---
+++ GuessGame/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace GuessGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("What is my number?");
+            int userGuess = Int32.Parse(Console.ReadLine());
+            bool correctGuess = false;
+
+            while (!correctGuess)
+            {
+                if (randomNumber == userGuess) 
+                {
+                    Console.WriteLine("You won!");
+                    correctGuess = true;
+                }else
+                {
+                    if(randomNumber > userGuess)
+                    {
+                        Console.WriteLine("My number is bigger.");
+                        Console.WriteLine("What is my number?");
+                        userGuess = Int32.Parse(Console.ReadLine());
+                    }
+                    else
+                    {
+                        Console.WriteLine("My number is smaller.");
+                        Console.WriteLine("What is my number?");
+                        userGuess = Int32.Parse(Console.ReadLine());
+                    }
+                       
+                }
+            }
+
+        }
+    }
+}
---
+++ ThreeTries/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ThreeTries
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int randomNumber = rnd.Next(1, 11);
+            Console.WriteLine("Wha is my numbr?");
+            int userInput = Int32.Parse(Console.ReadLine());
+
+            int counter = 0;
+            
+            while(counter < 3)
+            {
+                if(randomNumber == userInput)
+                {
+                    Console.WriteLine("Cangratulations, you won");
+                    break;
+
+                }
+                else
+                {
+                    Console.WriteLine("Try again!");
+                    Console.WriteLine("Wha is my numbr?");
+                    userInput = Int32.Parse(Console.ReadLine());
+
+
+                }
+                counter++;
+                Console.WriteLine($"you have guessed {counter} times.");
+
+            }
+        }
+    }
+}
